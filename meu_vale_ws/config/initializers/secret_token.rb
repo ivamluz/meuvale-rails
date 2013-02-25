35c1e932c5713f33bd8f662af8ae633bd8cb61b2
iluz <iluz@ciandt.com>
2013-02-25 20:06:02 -0300

    Add initial rails project

---
+++ meu_vale_ws/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeuValeWs::Application.config.secret_token = 'a464cf9c8bb0a631c156ad6341ee93b6c43fca16cf25cdca60f00fcedfcb1ab78ef60ae5a6d19f181622783c53fe0a25c095645b016ccbeec1340efd861633d7'
