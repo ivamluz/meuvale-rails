a3a3b4849559e99267985d377ee0c2643d6c3cfb
iluz <iluz@ciandt.com>
2013-04-06 00:13:25 -0300

    Removed unnecessary handler for 500 error

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -17,18 +17,6 @@ class CardsController < ActionController::Base
           logger.error ex
 
           raise ActiveRecord::RecordNotFound
-
-          # render :json => {
-          #   error: 404,
-          #   message: 'Not found'
-          # }, :status => '404'
-        rescue => ex
-          logger.error ex
-
-          render :json => {
-            error: 500,
-            message: 'Internal server error'
-          }, :status => '500'
         end
       end
     end
